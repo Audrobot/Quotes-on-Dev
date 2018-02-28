bfc7039b51c0fe9bd4ef08ddd7443303d4e13260
Audrobot <kornickiaa@gmail.com>
2018-02-28 11:47:24 -0800

    modify localize script

--- functions.php
+++ functions.php
@@ -64,13 +64,15 @@ function qod_scripts() {
 	'/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
 
 	if( function_exists( 'rest_url' ) ) {
-		wp_enqueue_script( 'qod_api', get_template_directory_uri() . 'build/js/api.min.js',
+		wp_enqueue_script( 'qod_api', get_template_directory_uri() . '/build/js/api.min.js',
 		array('jquery'), false, true );
 
 		wp_localize_script( 'qod_api', 'api_vars', array(
-				'root_url' => esc_url( rest_url() ),
-				'home_url' => esc_url( home_url() ),
-
+				'root_url' => esc_url_raw( rest_url() ),
+				'home_url' => esc_url_raw( home_url() ),
+				'nonce' => wp_create_nonce(  'wp_rest' ),
+				'success' => 'Thanks, your submission was received',
+				'failure' => 'Your submission could not be processed',
 		) );
 
 	}
