b0b074bd18a79bdf95d1ebd22a8c40ca19198693
Audrobot <kornickiaa@gmail.com>
2018-03-02 11:19:02 -0800

    add on click function, add ajax request

--- js/api.js
+++ js/api.js
@@ -1,10 +1,44 @@
+//fetch a random quote post
+
+//history api History.pushState()
+
+//submit a new quote with the form using ajax
+
+
+
 (function($) {
 
+  //fetch a random quote post on click
+  $('#new-quote-button').on('click', function(event) {
+    event.preventDefault();
 
-  //fetch a random quote post
-  //history api History.pushState()
+    $.ajax( {
+      url: 'wp-json/wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+      success: function( data ) {
 
-  //submit a new quote with the form using ajax
+        //returns the first post from the array
+        var post = data.shift();
+
+        //replace the current quote with the new quote
+        $('.entry-content').html( post.content.rendered );
+        $( '.entry-title' ).html( '&mdash; ' + post.title.rendered );
+
+        //if the source is available, output it, else output an empty string to fill it
+        if ( typeof post._qod_quote_source !== 'undefined') {
+          $( '.source' ).html(', <a href="' + post._qod_quote_source_url + '">' + post._qod_quote_source + '</a>');
+        
+        } else {
+          $( '.source' ).text('');
+        }
+
+      }
+    
+    })
+  
+  })
+
+})(jQuery);
 
 
-}) (jQuery);
+
+
