ddd3c90150778b1b576a497d32821743c9958d61
Audrobot <kornickiaa@gmail.com>
2018-03-02 14:32:03 -0800

    add history push state, pop state

--- js/api.js
+++ js/api.js
@@ -1,41 +1,51 @@
-//fetch a random quote post
-
-//history api History.pushState()
-
-//submit a new quote with the form using ajax
-
-
 
 (function($) {
 
-  //fetch a random quote post on click
+  var lastPage='';
   $('#new-quote-button').on('click', function(event) {
     event.preventDefault();
 
     $.ajax( {
-      url: 'wp-json/wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+      url: '/quotes_on_dev/wp-json/wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1',
       success: function( data ) {
 
         //returns the first post from the array
         var post = data.shift();
-
+        
         //replace the current quote with the new quote
         $('.entry-content').html( post.content.rendered );
         $( '.entry-title' ).html( '&mdash; ' + post.title.rendered );
 
         //if the source is available, output it, else output an empty string to fill it
-        if ( typeof post._qod_quote_source !== 'undefined') {
+        if ( post._qod_quote_source !== '' && post._qod_quote_source_url !== '') {
           $( '.source' ).html(', <a href="' + post._qod_quote_source_url + '">' + post._qod_quote_source + '</a>');
         
-        } else {
+        } 
+        else if (post._qod_quote_source !== '') {
+          $( '.source' ).text(', ' + post._qod_quote_source);
+        }
+
+        else {
           $( '.source' ).text('');
         }
+
+        history.pushState(null, null, post.link);
 
       }
     
     })
   
   })
+
+  $(window).on('popstate', function() {
+    console.log("popstate active!");
+    if (window.location.hash.indexOf('qm-overview') === 1) {
+      return false;
+    }else {
+      window.location.replace(lastPage);
+    }
+  
+  });
 
 })(jQuery);
 
